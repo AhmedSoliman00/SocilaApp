77e25531c4b5b8d73be49e965f25f461d0373265
Ahmed Soliman <himozhima@gmail.com>
2023-12-12 00:10:07 +0200

    protected route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import {
   RouterProvider,
   Route,
   Outlet,
+  Navigate,
 } from "react-router-dom";
 import NavBar from "./components/navBar/NavBar";
 import LeftBar from "./components/leftBar/LeftBar";
@@ -13,6 +14,7 @@ import RightBar from "./components/rightBar/RightBar";
 import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
 function App() {
+  const currentUser = false;
   const Layout = () => {
     return (
       <div>
@@ -25,14 +27,26 @@ function App() {
       </div>
     );
   };
+
+  // protected route
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
+    }
+    return children; // you must be 
+  };
+
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Layout />,
+      element: (
+        <ProtectedRoute>
+          <Layout />
+        </ProtectedRoute>
+      ),
       children: [
         { path: "/", element: <Home /> },
         { path: "/profile/:id", element: <Profile /> },
-        
       ],
     },
     {
